a37a34ec9ae49ab3daf86e16418e00e9f05d444c
Destiny Boone <destaj.db@gmail.com>
2022-11-07 16:05:00 -0500

    setting and patching form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,37 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // @ViewChild('f') signupForm: NgForm; // could also use viewchild to gain access to form
+  @ViewChild('f') signupForm: NgForm; // could also use viewchild to gain access to form
+  // signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // this.form.setValue({ // would have to decide all input fields; not best approach
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    this.signupForm.form.patchValue({ // this would be a better approach because we can change specific inputs individually without having to overwrite every input field
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
-  onSubmit(form: NgForm){
-    console.log(form);
-  }
-
-  // onSubmit(signupForm){ // could also use viewchild to gain access to form
-  //   console.log()
+  // onSubmit(signupForm: NgForm){
+  //   console.log(signupForm);
   // }
+
+  onSubmit(signupForm: NgForm){ // could also use viewchild to gain access to form
+    console.log(signupForm);
+  }
 }
