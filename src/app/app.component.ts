6b20cb8914d3b0a765382d107e4623d1f044b7e7
Destiny Boone <destaj.db@gmail.com>
2022-11-07 15:16:23 -0500

    accessing the form with viewchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,6 +7,8 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // @ViewChild('f') signupForm: NgForm; // could also use viewchild to gain access to form
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
@@ -14,4 +16,8 @@ export class AppComponent {
   onSubmit(form: NgForm){
     console.log(form);
   }
+
+  // onSubmit(signupForm){ // could also use viewchild to gain access to form
+  //   console.log()
+  // }
 }
