f8ddfd4bb515e366739948b9aff41ce757519fd8
Destiny Boone <destaj.db@gmail.com>
2022-11-07 15:34:39 -0500

    set default values with ngModel property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   // @ViewChild('f') signupForm: NgForm; // could also use viewchild to gain access to form
+  defaultQuestion = 'pet';
 
   suggestUserName() {
     const suggestedName = 'Superuser';
